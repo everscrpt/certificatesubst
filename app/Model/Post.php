62fd86b39eef994a7792e39645de5a71511d8441
Shift <shift@laravelshift.com>
2024-06-15 19:07:01 +0000

    Convert deprecated `$dates` property to `$casts`

--- app/Model/Post.php
+++ app/Model/Post.php
@@ -11,10 +11,6 @@ class Post extends Model
 {
     use Sluggable, SluggableScopeHelpers, SoftDeletes;
 
-    protected $dates = [
-        'deleted_at',
-    ];
-
     protected $fillable = [
         'post_author', 'post_content', 'post_title', 'featured_image', 'slug', 'post_excerpt', 'post_status', 'comment_status', 'post_parent', 'post_type', 'post_mime_type', 'comment_count',
     ];
